9df249b17d1d688a29f09c962175c754f06c3cd1
Namees Albayati <nameesalbayati@fb.com>
2023-01-11 16:24:58 -0600

    push git init

---
+++ public/scripts/script.js
@@ -0,0 +1,36 @@
+const nameEl=document.getElementById('nameId');
+const emailEl=document.getElementById('emailId');
+const reviewEl=document.getElementById('reviewId');
+const rateEl=document.getElementById('rateId');
+
+const fetchData=(data)=>
+    fetch('/api',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(data),
+
+    }).then((res)=>{
+ return res.json()}).then((data)=>{
+return data}).catch((err)=>{
+    throw err
+})
+
+document.getElementById('submitId').addEventListener('click',(e)=>{
+   e.preventDefault()
+    var userData={
+        name:nameEl.value.trim(),
+        email:emailEl.value.trim(),
+        rate:rateEl.value.trim(),
+        review:reviewEl.value.trim()
+
+    }
+  fetchData(userData).then((d)=>{
+    console.log(d.id)
+window.alert(`${d.id} has been added `)  }).catch((err)=>{
+    console.log(err)
+  })
+
+
+})
---
+++ server.js
@@ -0,0 +1,66 @@
+const express=require('express')
+const app=express();
+const path=require('path')
+const livereload=require('livereload');
+const connectLivereload=require('connect-livereload');
+const uuid = require('./helpers/uuid');
+const liveReloadServer=livereload.createServer();
+var publicdir=path.join(__dirname,'public');
+liveReloadServer.watch(publicdir)
+app.use(connectLivereload());
+var dataArr=[]
+var fs=require('fs')
+
+app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/index.html'))
+})
+app.post('/api',(req,res)=>{
+
+const uuidVal=uuid()
+const newReview={
+    name:req.body.name,
+    email:req.body.email,
+    rate:req.body.rate,
+    review:req.body.review,
+    id:uuidVal
+
+ }
+
+ fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const parsedReviews = JSON.parse(data);
+
+      // Add a new review
+      parsedReviews.push(newReview);
+
+      // Write updated reviews back to the file
+      fs.writeFile(
+        './db/reviews.json',
+        JSON.stringify(parsedReviews, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated reviews!')
+      );
+    }
+  });
+
+
+
+
+
+    res.status(201).json({
+        message:"received from fetch",
+        id:uuidVal
+    });
+})
+
+app.listen(3001,()=>{
+    console.log('listening')
+})
