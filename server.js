c7e159d1462d787099a6cac18537f41e41288115
Namees Albayati <nameesalbayati@fb.com>
2023-01-11 16:31:13 -0600

    second push

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const app=express();
+const PORT = process.env.PORT || 3001;
 const path=require('path')
 const livereload=require('livereload');
 const connectLivereload=require('connect-livereload');
@@ -61,6 +62,6 @@ const newReview={
     });
 })
 
-app.listen(3001,()=>{
+app.listen(PORT,()=>{
     console.log('listening')
 })
