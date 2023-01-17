4a939b45e2639af3ce70ee8fb3bb7c49336ef007
Namees Albayati <nameesalbayati@fb.com>
2023-01-17 17:00:40 -0600

    adding carsoul

--- public/scripts/scripthome.js
+++ public/scripts/scripthome.js
@@ -1,23 +1,30 @@
 const carsoulContainr=document.getElementById('container');
 function createCar(data){
-    console.log('fun',data)
+    console.log('fun',data.description)
 const div1=document.createElement('div');
+var link=document.createElement('a')
+link.setAttribute('href',data.github)
 div1.classList.add('carousel-item','add')
 
 const image=document.createElement('img');
 image.src=`../images/${data.image}`;
 image.classList.add('d-block','w-100','h-100')
+
 const div2=document.createElement('div');
 div2.classList.add("carousel-caption","d-none","d-md-block")
-var head5=document.createElement('h5');
+var head5=document.createElement('a');
+head5.setAttribute('href',data.github)
 head5.textContent=data.name
 
 var paragraph=document.createElement('p');
 paragraph.textContent=data.description
-div1.appendChild(image)
-div1.appendChild(div2)
+head5.appendChild(link)
 div2.appendChild(head5)
 div2.appendChild(paragraph)
+div1.appendChild(image)
+
+div1.appendChild(div2)
+
 carsoulContainr.appendChild(div1)
 }
 const getData=()=>
