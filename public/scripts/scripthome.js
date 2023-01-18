6ea47b205f1946054b4e2719d9ea0ffe47ecb9b6
Namees Albayati <nameesalbayati@fb.com>
2023-01-18 16:34:55 -0600

    done

--- public/scripts/scripthome.js
+++ public/scripts/scripthome.js
@@ -3,7 +3,11 @@ function createCar(data){
     console.log('fun',data.description)
 const div1=document.createElement('div');
 var link=document.createElement('a')
+const icon=document.createElement('p');
+icon.innerHTML = `<a href='${data.github}'><i class='fab fa-github 'style='font-size:36px;color:white;' ></i></a> <a href='${data.url}'><i class='fas fa-eye' style='font-size:48px;color:white'></i></a>`;
+
 link.setAttribute('href',data.github)
+link.setAttribute('style','outline: none;border: 0;')
 div1.classList.add('carousel-item','add')
 
 const image=document.createElement('img');
@@ -12,15 +16,20 @@ image.classList.add('d-block','w-100','h-100')
 
 const div2=document.createElement('div');
 div2.classList.add("carousel-caption","d-none","d-md-block")
-var head5=document.createElement('a');
+div2.setAttribute('style','margin-bottom:100px;height:10px;')
+var head5=document.createElement('h4');
 head5.setAttribute('href',data.github)
 head5.textContent=data.name
 
-var paragraph=document.createElement('p');
+var paragraph=document.createElement('h6');
 paragraph.textContent=data.description
+paragraph.setAttribute('style','color: yellow;font-size:small')
 head5.appendChild(link)
-div2.appendChild(head5)
+div2.appendChild(icon)
+
 div2.appendChild(paragraph)
+div2.appendChild(head5)
+
 div1.appendChild(image)
 
 div1.appendChild(div2)
